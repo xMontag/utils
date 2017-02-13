6bbcce13364cb92b05317d98d7b1685a2e0706a7
GitMontag <GuyVMontag@gmail.com>
2017-02-13 11:39:20 +0200

    init

---
+++ utils/ModelUtils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Geometry3d;
+
+namespace utils
+{
+    public class ModelUtils
+    {
+		public static void CheckOneBoltGroup()
+        {
+            Picker picker = new Picker();            
+            BoltGroup myBoltGroup = picker.PickObject(Picker.PickObjectEnum.PICK_ONE_BOLTGROUP, "pick BOLT GROUP") as BoltGroup;
+			double boltLength = 0,
+			       boltDiameter = 0;
+			int boltNParts = 1;
+			myBoltGroup.GetReportProperty("BOLT_NPARTS", ref boltNParts);
+			string[] boltPartGUID = new string[boltNParts];
+			myBoltGroup.GetReportProperty("MAIN_PART.GUID", ref boltPartGUID[0]);
+			if (boltNParts > 1)
+			{
+				for (int i = 1; i < boltNParts; i++)
+				{
+					myBoltGroup.GetReportProperty("SECONDARY_" + i + ".GUID", ref boltPartGUID[i]);
+				}
+			}
+			myBoltGroup.GetReportProperty("LENGTH",ref boltLength);
+			myBoltGroup.GetReportProperty("DIAMETER", ref boltDiameter);
+
+
+
+			Console.WriteLine(boltNParts + "\n" +
+							  boltDiameter + "\n" +
+							  boltLength);
+			foreach (String GUID in boltPartGUID)
+			{
+				Console.WriteLine(GUID);
+			}
+			Console.WriteLine(myBoltGroup.BoltStandard);
+			
+        }
+
+
+    }
+}
---
+++ utils/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utils
+{
+    public static class Program
+    {
+        public static bool IsApp = false;
+        [STAThread]
+        
+        public static void Main()
+        {
+            IsApp = true;
+            ModelUtils.CheckOneBoltGroup();
+        }
+
+    }
+}
